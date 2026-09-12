8d26f54b184e7f91f1ffa3de30420180fc0ea0e0
Tiago <tiagomatias072@gmail.com>
2026-09-12 19:44:38 +0100

    fix: add suppressHydrationWarning to layout root elements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt" suppressHydrationWarning>
+      <body className={inter.className} suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
